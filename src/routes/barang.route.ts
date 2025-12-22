e599202393725dfe7d2c5eac541a6c533a61b45f
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-12-22 21:33:45 +0700

    feat: add input validation for barang and define BarangType interface

--- src/routes/barang.route.ts
+++ src/routes/barang.route.ts
@@ -1,9 +1,28 @@
 import { Router, type Request, type Response } from 'express'
+import { inputBarangValidation } from '../validations/barang.validation.js'
 
 const barangRouter = Router()
 
 barangRouter.get('/barang', (req: Request, res: Response) => {
   res.status(200).json({ message: 'List of barang' })
+})
+
+barangRouter.post('/barang', (req: Request, res: Response) => {
+  const { error, value } = inputBarangValidation(req.body)
+  if (error != null) {
+    return res.status(400).json({
+      //error: error.details.map((detail) => detail.message),
+      error: error.details.map,
+      message: 'Validasi input barang gagal',
+      data: value
+    })
+  }
+
+  return res.status(200).json({
+    error: null,
+    message: 'Validasi input barang berhasil',
+    data: value
+  })
 })
 
 export default barangRouter
---
+++ src/validations/barang.validation.ts
@@ -0,0 +1,29 @@
+import joi from 'joi'
+import type BarangType from '../types/BarangType.js'
+
+export const inputBarangValidation = (
+  payload: BarangType
+): joi.ValidationResult<BarangType> => {
+  const schema = joi
+    .object<BarangType>({
+      nama: joi.string().trim().required().messages({
+        'string.base': 'nama harus berupa string',
+        'string.empty': 'nama tidak boleh kosong',
+        'any.required': 'nama harus diisi'
+      }),
+      jumlah: joi.number().required().messages({
+        'number.base': 'jumlah harus berupa angka',
+        'number.empty': 'jumlah tidak boleh kosong',
+        'any.required': 'jumlah harus diisi'
+      }),
+      harga: joi.number().required().messages({
+        'number.base': 'harga harus berupa angka',
+        'number.empty': 'harga tidak boleh kosong',
+        'any.required': 'harga harus diisi'
+      })
+    })
+    .required()
+
+  //return schema.validate(payload, { abortEarly: false })
+  return schema.validate(payload)
+}

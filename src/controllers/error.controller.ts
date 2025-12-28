eb200c4f156e8381bd0e93ead50ffc3d3e5eed79
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-12-28 22:12:11 +0700

    feat: add error handling and not found middleware to the router

---
+++ src/controllers/error.controller.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from 'express'
+import logger from '../utils/winston'
+
+export const errorHandling = (
+  err: Error,
+  req: Request,
+  res: Response
+): Response | void => {
+  const parts = err.message.split(' - ')
+  const message = parts.length > 1 ? parts[1] : err.message
+  logger.error(err)
+  res.status(500).json({
+    error: message,
+    message: 'internal server error',
+    data: null
+  })
+}
+
+export const notFound = (req: Request, res: Response): Response | void => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: 'Halaman tidak ditemukan',
+    data: null
+  })
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express'
 import barangRouter from './barang.route'
+import { errorHandling, notFound } from '../controllers/error.controller'
 
 const router = Router()
 
 //http://localhost:3000/api/barang
+
+// 1. Middleware Pertama
 router.use('/api', barangRouter)
+
+// 2. Middleware Kedua (Standard)
+router.use(notFound)
+
+// 3. Middleware Ketiga (Error Handler)
+router.use(errorHandling)
 
 export default router

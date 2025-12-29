2479d2c2cbe1e838b57949c6cb0163fcaec4c92f
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-12-29 21:26:29 +0700

    fix: add missing next function parameter to error handling middleware

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,10 +1,15 @@
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import logger from '../utils/winston'
 
 export const errorHandling = (
   err: Error,
   req: Request,
-  res: Response
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+  //Ubah nama next menjadi _next (atau _). Konvensi ESLint biasanya mengabaikan variabel yang diawali dengan underscore.
+  // Express.js menentukan apakah sebuah middleware adalah "Error Handler" atau "Standard Middleware" semata-mata berdasarkan jumlah argumen (parameter) yang didefinisikan dalam fungsi tersebut, bukan berdasarkan nama variabelnya.  3 Argumen (req, res, next): Express menganggapnya middleware biasa.4 Argumen (err, req, res, next): Express menganggapnya Error Handler.
+  // Jadi, jika Anda menulis (err, req, res, _next), jumlah argumennya tetap 4. Express akan tetap mengenalinya sebagai error handler. Penggunaan underscore (_) di awal nama variabel (_next) hanyalah konvensi di TypeScript/JavaScript untuk memberi tahu linter (seperti ESLint) bahwa variabel tersebut sengaja tidak digunakan, sehingga tidak memunculkan warning atau error.
 ): Response | void => {
   const parts = err.message.split(' - ')
   const message = parts.length > 1 ? parts[1] : err.message

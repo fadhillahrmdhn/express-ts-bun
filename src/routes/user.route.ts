c5320fff0a694c4e7872ec8b72b1269da4517590
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2026-01-01 23:25:01 +0700

    fix: remove duplicate comment for register route in user router

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,7 @@ import { registerUser } from '../controllers/user.controller'
 
 const router = Router()
 
+//http://localhost:4000/api/register
 router.post('/register', registerUser)
 
 export default router

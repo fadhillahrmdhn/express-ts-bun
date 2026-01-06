ea4e9a305f9442365bebe2919909c029ae0eab62
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2026-01-07 05:37:58 +0700

    feat: add login route to user routes for user authentication

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller'
+import { loginUser, registerUser } from '../controllers/user.controller'
 
 const router = Router()
 
 //http://localhost:4000/api/register
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 export default router

393890b17723e5cd4c07183af538ec79a143c5b4
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2026-01-08 22:31:30 +0700

    feat: refactor JWT utility functions to use User interface for token generation and verification

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,17 @@
 import jsonWebToken from 'jsonwebtoken'
 import type { SignOptions } from 'jsonwebtoken'
-interface JwtPayload {
+import type { JwtPayload } from 'jsonwebtoken'
+
+export interface User {
   id: string
   email: string
   nama: string
   role: string
 }
 
-const generateAccessToken = (user: JwtPayload): string => {
+export interface UserPayload extends User, JwtPayload {}
+
+const generateAccessToken = (user: User): string => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined')
   }
@@ -20,7 +24,7 @@ const generateAccessToken = (user: JwtPayload): string => {
   return jsonWebToken.sign(user, process.env.JWT_SECRET, options)
 }
 
-const generateRefreshToken = (user: JwtPayload): string => {
+const generateRefreshToken = (user: User): string => {
   if (!process.env.JWT_REFRESH_SECRET) {
     throw new Error('JWT_REFRESH_SECRET is not defined')
   }
@@ -31,4 +35,45 @@ const generateRefreshToken = (user: JwtPayload): string => {
   return jsonWebToken.sign(user, process.env.JWT_REFRESH_SECRET, options)
 }
 
-export { generateAccessToken, generateRefreshToken }
+const verifyToken = (token: string): UserPayload | null => {
+  if (!process.env.JWT_REFRESH_SECRET) {
+    throw new Error('JWT_REFRESH_SECRET is not defined')
+  }
+  try {
+    return jsonWebToken.verify(
+      token,
+      process.env.JWT_REFRESH_SECRET
+    ) as UserPayload
+  } catch {
+    return null
+  }
+}
+
+const parseJWT = (token: string): UserPayload => {
+  const base64Payload = token.split('.')[1]
+  if (!base64Payload) {
+    throw new Error('Invalid token')
+  }
+  return JSON.parse(
+    Buffer.from(base64Payload, 'base64').toString()
+  ) as UserPayload
+}
+
+const verifyAccessToken = (token: string): UserPayload | null => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not defined')
+  }
+  try {
+    return jsonWebToken.verify(token, process.env.JWT_SECRET) as UserPayload
+  } catch {
+    return null
+  }
+}
+
+export {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyToken,
+  parseJWT,
+  verifyAccessToken
+}
